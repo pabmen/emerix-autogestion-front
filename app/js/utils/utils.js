fc6ec1568338f48de51a3c7fe426338dd2dc2fd6
Pablo Mendoza <pablo.mendoza@wundermanthompson.com>
2020-06-28 02:00:28 -0300

    Templating w/ macros advance

--- app/js/utils/utils.js
+++ app/js/utils/utils.js
@@ -37,83 +37,6 @@ function throttle(fn, threshhold, scope) {
 	};
 }
 
-function timeDifference(current, previous) {
-    const msPerMinute = 60 * 1000;
-    const msPerHour = msPerMinute * 60;
-    const msPerDay = msPerHour * 24;
-    const msPerMonth = msPerDay * 30;
-    const msPerYear = msPerDay * 365;
-	const elapsed = current - previous;
-
-	let type, value;
-
-    if (elapsed < msPerMinute) {
-		type = 'seconds'
-		value = Math.round(elapsed/1000)
-    } else if (elapsed < msPerHour) {
-		type = 'minutes'
-		value = Math.round(elapsed/msPerMinute)
-    } else if (elapsed < msPerDay ) {
-		type = 'hours'
-		value = Math.round(elapsed/msPerHour )
-    } else if (elapsed < msPerMonth) {
-		type = 'days'
-		value = Math.round(elapsed/msPerDay)
-    } else if (elapsed < msPerYear) {
-		type = 'months'
-		value = Math.round(elapsed/msPerMonth)
-    } else {
-		type = 'years'
-		value = Math.round(elapsed/msPerYear)
-	}
-
-	return {
-		type,
-		value
-	}
-}
-
-/**
- * Use with animate.css
- * See https://github.com/daneden/animate.css#usage-with-javascript
- * @param element
- * @param animationName
- * @param [callback]
- */
-function animateCSS(element, animationName, callback) {
-	const node = (typeof element === 'string') ? document.querySelector(element) : element;
-	node.classList.add('animated', animationName);
-
-	function handleAnimationEnd() {
-		node.classList.remove('animated', animationName);
-		node.removeEventListener('animationend', handleAnimationEnd);
-
-		if (typeof callback === 'function') callback();
-	}
-
-	node.addEventListener('animationend', handleAnimationEnd);
-}
-
-
-/**
- *
- * @param buttonElem
- */
-function enableButton(buttonElem) {
-	buttonElem.removeAttribute('disabled');
-}
-
-
-/**
- *
- * @param buttonElem
- */
-function disableButton(buttonElem) {
-	buttonElem.setAttribute('disabled', 'disabled');
-}
-
-
-
 /**
  * Checks device orientation, landscape => true or portrait => false
  * @param callback
@@ -134,9 +57,5 @@ function checkOrientation(callback) {
 export {
 	throttle,
 	parameter,
-	timeDifference,
-	animateCSS,
-	enableButton,
-	disableButton,
 	checkOrientation,
 }
